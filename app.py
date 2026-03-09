4479a226ebfa8dd5ab08327461acf9dce0cbe134
elixhir <elixhir365@gmail.com>
2026-03-09 05:10:32 -0400

    feat(master): compare changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_migrate import Migrate
+from flask_cors import CORS
 from src.infrastructure.database import db
 from src.infrastructure.models import *
 from src.api.v1.store import store_bp
@@ -14,6 +15,8 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+CORS(app)
 
 db.init_app(app)
 migrate = Migrate(app, db)
