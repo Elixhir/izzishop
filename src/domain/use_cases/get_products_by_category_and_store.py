355d0e37aaa17b2d1128d0946bced2252485f94a
elixhir <elixhir365@gmail.com>
2026-03-10 10:31:01 -0400

    feat(master): compare changes

--- src/domain/use_cases/get_products_by_category_and_store.py
+++ src/domain/use_cases/get_products_by_category_and_store.py
@@ -1,8 +1,8 @@
 from src.domain.interfaces.product import ProductInterface
 
 class GetProductsByCategoryAndStore:
-    def __init__(self, product_interface: ProductInterface):
-        self.product_interface = product_interface
+    def __init__(self, product_repository: ProductInterface):
+        self.product_repository = product_repository
 
     def execute(self, category_id: int, store_id: int) -> list:
-        return self.product_interface.get_products_by_category_and_store(category_id, store_id)
+        return self.product_repository.get_products_by_category_and_store(category_id, store_id)
