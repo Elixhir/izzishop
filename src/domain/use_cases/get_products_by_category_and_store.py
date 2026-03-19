91216af3b96a0ba6d2518f4d8bd8eabf745cab98
elixhir <elixhir365@gmail.com>
2026-03-19 09:13:03 -0400

    feat(master): compare changes

--- src/domain/use_cases/get_products_by_category_and_store.py
+++ src/domain/use_cases/get_products_by_category_and_store.py
@@ -4,13 +4,13 @@ class GetProductsByCategoryAndStore:
     def __init__(self, product_repository: ProductInterface):
         self.product_repository = product_repository
 
-    def execute(self, category_id: int, store_id: int, page: int = 1, per_page: int = 10) -> list:
-        offset = (page - 1) * per_page
+    def execute(self, category_id: str, store_id: str, page: int = 1, per_page: int = 10):
         
         products, total = self.product_repository.get_products_by_category_and_store(
             category_id=category_id,
             store_id=store_id,
-            offset=offset,
-            limit=per_page
+            page=page,   
+            per_page=per_page 
         )
+        
         return products, total
--- src/domain/use_cases/get_products_by_store.py
+++ src/domain/use_cases/get_products_by_store.py
@@ -5,12 +5,11 @@ class GetProductsByStore:
         self.product_repository = product_repository
 
     def execute(self, store_id: str, page: int = 1, per_page: int = 10):
-        offset = (page - 1) * per_page
         
         products, total = self.product_repository.get_products_by_store(
             store_id=store_id,
-            offset=offset,
-            limit=per_page
+            page=page,      
+            per_page=per_page 
         )
         
         return products, total
